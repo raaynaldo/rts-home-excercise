0677fe9883d45d6341257af8070e69b646b3603b
raaynaldo <raaynaldo@hotmail.com>
2021-03-02 13:00:05 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+const App = () => {
+    const [historyActive, setHistoryActive] = useState(false);
+    return (
+        <div className='bg-gray-200'>
+            
+        </div>
+    );
+};
+
+export default App;
